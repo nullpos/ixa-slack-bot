7991d3bdd45fe7f55412f86d05603c9ca0f16fc4
nullpos <greenstar77@gmail.com>
2017-02-12 21:33:31 +0900

    update

--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ class MapCoordinate
     @baseurl = 'http://y061.sengokuixa.jp/map.php?'
     @regexp = /([ー－‐―\-]?[０-９\d]+)[,，.．、､]\s?([ー－‐―\-]?[０-９\d]+)/
     @country = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    @country_name = ['織田', '明智', '武田', '相馬', '徳川', '毛利', '柴田', '北条', '長宗我部', '島津', '豊臣', '里見']
     @x, @y, @c = '0', '0', '2'
   end
 
@@ -17,7 +18,7 @@ class MapCoordinate
   end
 
   def normalize_coord(mes)
-    coord = mes[regexp, 0]
+    coord = mes[@regexp, 0]
     coord.gsub(/[ー－‐―\-]/, '-')
          .gsub(/[,，.．、､]/, ',')
          .tr('０-９', '0-9')
@@ -28,7 +29,7 @@ class MapCoordinate
     if (xy[0] == @x && xy[1] == @y)
       return true
     end
-    @x = xy[0], @y = xy[1]
+    @x, @y = xy[0], xy[1]
     false
   end
 
@@ -48,8 +49,9 @@ class MapCoordinate
     elsif @country.include?(set)
       @c = set
       return true
-    else
-      return false
+    elsif @country_name.include?(set)
+      @c = @country[@country_name.index(set)]
+      return true
     end
   end
 end
@@ -62,23 +64,22 @@ end
 client = Slack::RealTime::Client.new
 
 client.on :hello do
-  puts 'Successfully connected, welcome "#{client.self.name}" to the "#{client.team.name}" team at https://#{client.team.domain}.slack.com.'
+  puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
 end
 
+map_coord = MapCoordinate.new
 client.on :message do |data|
   if data.type == 'message' && data.user != client.self.id
-    map_coord = MapCoordinate.new
 
-    case data.text
-    when map_coord.coord_mes?(data.text) then
+    if map_coord.coord_mes?(data.text)
       xy = map_coord.normalize_coord(data.text)
       if !map_coord.same_coord?(xy) && !map_coord.include_url?(data.text)
         client.message channel: data.channel, text: map_coord.create_url
       end
-    when map_coord.cset_mes?(data.text) then
+    elsif map_coord.cset_mes?(data.text)
       if map_coord.set_c(data.text)
         client.message channel: data.channel,
-                       text: 'successfully set country id to ' + set
+                       text: 'successfully set country id'
       else
         client.message channel: data.channel, text: 'unable to set country id'
       end
